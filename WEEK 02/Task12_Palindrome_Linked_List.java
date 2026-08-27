f92683900d16dd28a8f157b81adb645789c57cc7
venkatakrishnasagala-cell <venkatakrishnasagala@gmail.com>
2026-08-27 10:26:55 +0530

    Add files via upload

---
+++ WEEK 02/Task12_Palindrome_Linked_List.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverse(slow);
+        ListNode firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
---
+++ WEEK 02/Task14_Merge_K_Sorted_Lists.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.offer(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
